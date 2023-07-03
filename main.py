9e83810fc2f0e1c5f544eff0d2def6cf40d639b4
Giovanni Limena <giovanni.limena@publicissapient.com>
2023-07-03 16:25:54 +0100

    empty board grid

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello")
+import numpy as np
+import termtables as tt
+
+puzzle = np.array([[" "," "," "," "," "," "," "," "], [" "," "," "," "," "," "," "," "], [" "," "," "," "," "," "," "," "], [" "," "," "," "," "," "," "," "], [" "," "," "," "," "," "," "," "], [" "," "," "," "," "," "," "," "], [" "," "," "," "," "," "," "," "]])
+
+puzzle_arr = np.array(puzzle).reshape(-1, 8)
+
+puzzle_arr
+grid = tt.to_string(
+    puzzle_arr,
+    style=tt.styles.ascii_thin,
+    # alignment="ll",
+    # padding=(0, 1),
+)
+print(grid)
