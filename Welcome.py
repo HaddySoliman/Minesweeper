f148a31679c201a926e48f42e42c082daa137b7d
Noor Naseer <noor.naseer@publicissapient.com>
2023-07-04 10:06:20 +0100

    To test connecting functions

---
+++ Welcome.py
@@ -0,0 +1,8 @@
+def welcomeInstructions ():
+    input("Welcome to Minesweeper! Press enter to view the instructions...")
+    my_string = '''The game begins on a grid of unmarked squares - click on one to start the game. 
+    You will see that some squares will disappear, others will now contain numbers, and some just remain blank.
+    The numbers signify how many mines are '''
+    print(my_string)
+welcomeInstructions
+()
